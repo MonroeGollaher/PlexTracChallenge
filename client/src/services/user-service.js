ce933c38607c9fd95d1538d2ad70663c01343b89
Monroe Gollaher <mgollaher92@gmail.com>
2021-04-24 16:21:50 -0600

    remove unsued file

--- client/src/services/user-service.js
+++
@@ -1,9 +0,0 @@
-export async function getUsers(url) {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      resolve(data);
-    })
-  })
-}
